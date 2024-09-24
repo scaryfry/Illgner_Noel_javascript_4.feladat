e0e34840fd952f0587d0665806bf8ca763cabcb8
scaryfry <scaryfry@gmail.com>
2024-09-24 22:28:11 +0200

    halj meg

---
+++ feladat_7/for_loop_04.js
@@ -0,0 +1,9 @@
+function megjelenites(){
+
+    let word = document.getElementById('word');
+
+    for (let i = 0; i < word.length; i++) {
+        let element = word[i];
+        document.getElementById('output') = element;
+    }
+}
