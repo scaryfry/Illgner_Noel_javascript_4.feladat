55d708d45b90da205cfcfdd4f5331a2081a72b71
scaryfry <144095357+scaryfry@users.noreply.github.com>
2024-09-25 07:54:36 +0200

    Update for_loop_04.js

--- feladat_7/for_loop_04.js
+++ feladat_7/for_loop_04.js
@@ -3,7 +3,7 @@ function megjelenites(){
     let word = document.getElementById('word');
 
     for (let i = 0; i < word.length; i++) {
-        let element = word[i];
-        document.getElementById('output').innerHTML = element;
+        let element = word.charAt(i);
+        document.getElementById('output').innerHTML = `${element} <br>`;
     }
 }
